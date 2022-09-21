57e1cff34994f76c070f324e7d736665b1899ff0
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2022-09-21 16:40:32 +0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,13 +3,17 @@ local nicklim = 2
 local list = core.get_mod_storage()
 local whitelist = {} --not persistent = more secure
 
-local function get_ip(nick)
+local function get_ips(nick)
+    local ips = {}
     for ip,names in pairs(list:to_table().fields) do
         for _,name in pairs(names:split(" ")) do
             if name == nick then
-                return ip
+                table.insert(ips,ip)
             end
         end
+    end
+    if #ips > 0 then
+        return ips
     end
 end
 
@@ -36,9 +40,13 @@ core.register_chatcommand("ipinfo",{
   privs = {server=true},
   params = "<playername>",
   func = function(name,param)
-    local ip = get_ip(param)
-    if ip then
-        return true, "Accounts on "..param.."'s IP: "..list:get_string(ip)
+    local ips = get_ips(param)
+    local names = {}
+    if ips then
+        for _,ip in ipairs(ips) do
+            table.insert(names,list:get_string(ip))
+        end
+        return true, "Accounts on "..param.."'s IP: "..table.concat(names)
     end
     return false, "Nothing found"
 end})
@@ -50,13 +58,17 @@ core.register_chatcommand("iplim_wl",{
   func = function(name,param)
     local cmd,player = param:match("^(%S+) (.+)$")
     if not (cmd and player) then return false, "Invalid params" end
-    local ip = get_ip(player)
-    if not ip then return false, "Error getting player's IP" end
+    local ips = get_ips(player)
+    if not ips then return false, "Error getting player's IP" end
     if cmd == "add" then
-        whitelist[ip] = true
+        for _,ip in ipairs(ips) do
+            whitelist[ip] = true
+        end
         return true, player.." added to whitelist"
     elseif cmd == "rm" then
-        whitelist[ip] = nil
+        for _,ip in ipairs(ips) do
+            whitelist[ip] = nil
+        end
         return true, player.." removed from whitelist"
     else
         return false, "Invalid params"
